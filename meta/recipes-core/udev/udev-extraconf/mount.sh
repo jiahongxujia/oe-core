af356c57c0efce4da8a2502ce661a3e1f3d00578
Hongzhi.Song <hongzhi.song@windriver.com>
2018-03-12 09:21:20 -0400

    systemd-udev: Add systemd-mount to mount.sh of udev
    Issue: LIN10-3020
    
    Systemd upstream maintainer suggest us to use systemd-mount to mount
    hotpluggable media.
    
    systemd generally tries to run all our services with sandboxes that are as
    tight as we can make them. udev can run arbitrary stuff from its rules
    hence the sandbox can't be made too tight unfortunately.
    
    If we use systemd as init, systemd-udevd.service sets MountFlags=slave,
    which is essentially a sandboxing setting: it detaches mount() operations done
    within the service from the rest of the system. This means that, outside of
    namespace of systemd-udev, we can not access devices mounted by udev and some
    operation will be failed.(e.g. mkfs.ext4 /dev/sda1).
    
    Current automount.rules and mount.sh created for sysvinit is unable to solve
    above problems.
    
    This patch adds systemd-mount in /etc/udev/script/mount.sh. The tool recommended
    by systemd upstream generally is the best way in particular in embedded
    applications to deal with hotpluggable media. Systemd-mount request the mount
    operation to be executed by PID 1, and hence outside of the mount context of udev.
    
    (LOCAL REV: NOT UPSTREAM) -- Upstream status: submitted.
    
    Signed-off-by: Hongzhi.Song <hongzhi.song@windriver.com>

--- meta/recipes-core/udev/udev-extraconf/mount.sh
+++ meta/recipes-core/udev/udev-extraconf/mount.sh
@@ -4,10 +4,26 @@
 #
 # Attempt to mount any added block devices and umount any removed devices
 
+BASE_INIT="`readlink "/sbin/init"`"
+INIT_SYSTEMD="/lib/systemd/systemd"
 
-MOUNT="/bin/mount"
+if [ "x$BASE_INIT" = "x$INIT_SYSTEMD" ];then
+        MOUNT="/usr/bin/systemd-mount"
+        UMOUNT="/usr/bin/systemd-umount"
+
+        if [ -x $MOUNT ] && [ -x $UMOUNT ];
+        then
+                logger "Using systemd-mount to finish mount"
+        else
+                logger "Linux init is using systemd, so please install systemd-mount to finish mount"
+        fi
+else
+        MOUNT="/bin/mount"
+        UMOUNT="/bin/umount"
+fi
+
 PMOUNT="/usr/bin/pmount"
-UMOUNT="/bin/umount"
+
 for line in `grep -h -v ^# /etc/udev/mount.blacklist /etc/udev/mount.blacklist.d/*`
 do
 	if [ ` expr match "$DEVNAME" "$line" ` -gt 0 ];
@@ -16,6 +32,33 @@ do
 		exit 0
 	fi
 done
+
+automount_systemd() {
+	name="`basename "$DEVNAME"`"
+
+        ! test -d "/run/media/$name" && mkdir -p "/run/media/$name"
+        # Silent util-linux's version of mounting auto
+        MOUNT="$MOUNT -o silent"
+        
+        # If filesystem type is vfat, change the ownership group to 'disk', and
+        # grant it with  w/r/x permissions.
+        case $ID_FS_TYPE in
+        vfat|fat)
+                MOUNT="$MOUNT -o umask=007,gid=`awk -F':' '/^disk/{print $3}' /etc/group`"
+                ;;
+        # TODO
+        *)
+                ;;
+        esac
+
+        if ! $MOUNT --no-block -t auto $DEVNAME "/run/media/$name"
+        then
+                rm_dir "/run/media/$name"
+        else
+                logger "mount.sh/automount" "systemd-mount of [/run/media/$name] successful"
+                touch "/tmp/.automount-$name"
+        fi
+}
 
 automount() {	
 	name="`basename "$DEVNAME"`"
@@ -61,19 +104,21 @@ rm_dir() {
 # No ID_FS_TYPE for cdrom device, yet it should be mounted
 name="`basename "$DEVNAME"`"
 [ -e /sys/block/$name/device/media ] && media_type=`cat /sys/block/$name/device/media`
-
 if [ "$ACTION" = "add" ] && [ -n "$DEVNAME" ] && [ -n "$ID_FS_TYPE" -o "$media_type" = "cdrom" ]; then
 	if [ -x "$PMOUNT" ]; then
 		$PMOUNT $DEVNAME 2> /dev/null
 	elif [ -x $MOUNT ]; then
     		$MOUNT $DEVNAME 2> /dev/null
 	fi
-	
 	# If the device isn't mounted at this point, it isn't
 	# configured in fstab (note the root filesystem can show up as
 	# /dev/root in /proc/mounts, so check the device number too)
 	if expr $MAJOR "*" 256 + $MINOR != `stat -c %d /`; then
-		grep -q "^$DEVNAME " /proc/mounts || automount
+		if [ "`basename $MOUNT`" = "systemd-mount" ];then
+			grep -q "^$DEVNAME " /proc/mounts || automount_systemd
+		else
+			grep -q "^$DEVNAME " /proc/mounts || automount
+		fi
 	fi
 fi
 
